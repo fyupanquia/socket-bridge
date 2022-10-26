7f484db0884198c0e52155296c5d5e2cf0225925
fyupanquia <fyupanquia@outlook.com>
2022-10-25 20:46:09 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const PORT = process.env.PORT || 3002;
   await app.listen(PORT);
   console.log(`Server running on port ${PORT}`);
